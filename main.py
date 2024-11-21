2cdde0df9b2ff4d1a0ee9438fa2b02aad3374229
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-11-21 16:19:27 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+print(math.sqrt(4))
+print(math.pow(2,3))
+
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+print(math.ceil(490))
+print(math.floor(890))
+print(math.factorial(45))
